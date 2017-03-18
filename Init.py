1076e5a2eb7c2eab54f817dbb474afb2a5ce1542
yufeng0121 <yufeng19950121@gmail.com>
2017-03-19 02:46:55 +0800

    Init_function: input a k number list , and output a list contains k-1 matrix

--- Init.py
+++ Init.py
@@ -7,7 +7,11 @@ Created on Sun Mar 19 00:29:06 2017
 from numpy import *
 import numpy as np
 
-def Init(layerNum):
-    for i in range(0,len(layerNum)):
-        weight.append=mat(random.rand(layerNum[i],layerNum[i]))
+def Init(layerNum,weight=[]):
+    for i in range(0,len(layerNum)-1):
+        weight.append(mat(random.rand(layerNum[i],layerNum[i+1])))
     return weight
+
+layerNum=[3,4,5]
+weight=Init(layerNum)
+print(weight)
