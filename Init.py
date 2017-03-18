986c74a8e4463fb9c48fd584e7817a3d7312b2eb
yufeng0121 <yufeng19950121@gmail.com>
2017-03-19 01:41:16 +0800

    correction

--- Init.py
+++ Init.py
@@ -7,7 +7,7 @@ Created on Sun Mar 19 00:29:06 2017
 from numpy import *
 import numpy as np
 
-def Init(layer_num):
-    for i in range(0,len(layer_num)-1):
-        weight[i]=mat(random.rand(layer_num[i],layer_num[i]))
+def Init(layerNum):
+    for i in range(0,len(layerNum)):
+        weight.append=mat(random.rand(layerNum[i],layerNum[i]))
     return weight
