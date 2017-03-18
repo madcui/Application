a790cb65ae3668fdce7032e7253481cf028a5e8d
yufeng0121 <yufeng19950121@gmail.com>
2017-03-19 06:21:10 +0800

    add nonliner function

--- FunGetOutput.py
+++ FunGetOutput.py
@@ -7,20 +7,20 @@ Created on Sun Mar 19 02:52:32 2017
 
 from numpy import *
 import numpy as np
+from FunNeuronNLFun import NeuronNLFun
 
 # all inputs are list type variable
 def GetOutput(weight,inputData,neuronValue=[]):
-    inputData=mat(inputData)
+    inputData=NeuronNLFun(mat(inputData))
     neuronValue.append(inputData)
     for i in range(0,len(weight)):
         # retrive the last vector in the neuronValue list to generate next layer's value
-        neuronValue.append(mat(neuronValue[-1])*weight[i])
+        neuronValue.append(NeuronNLFun(mat(neuronValue[-1])*weight[i]))
     return neuronValue
 
-"""
+
 from FunInit import Init
 layerNum=[2,3,4]
 inputData=[1,2]
 weight=Init(layerNum)
 O=GetOutput(weight,inputData)
-"""
