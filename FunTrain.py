57292306c7ce454aa13ce93a9587e69adb8e6ce6
guangwei-si <siguangwei@gmail.com>
2017-03-18 15:55:46 -0400

    Created file FunTrain

---
+++ FunTrain.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 18 15:53:26 2017
+
+@author: guangwei
+"""
+
+def Train(wInit, trainMIn, trainMOut, param):
+    return wTrain
